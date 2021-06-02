3d51890bcd993a57e09c9c70c07fec823f2ddd8e
ilhoon <rival6821@naver.com>
2021-06-02 23:15:17 +0900

    Add Update Delete

--- main.go
+++ main.go
@@ -6,11 +6,37 @@ import (
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "first word"}
-	definition, err := dictionary.Search("first")
-	if err != nil {
-		fmt.Println(err)
+	dictionary := mydict.Dictionary{}
+	err1 := dictionary.Add("first", "first word")
+	if err1 != nil {
+		fmt.Println(err1)
+	} else {
+		fmt.Println(dictionary)
+	}
+	definition, err2 := dictionary.Search("first")
+	if err2 != nil {
+		fmt.Println(err2)
 	} else {
 		fmt.Println(definition)
+	}
+	err3 := dictionary.Add("second", "second word")
+	if err3 != nil {
+		fmt.Println(err3)
+	} else {
+		fmt.Println(dictionary)
+	}
+
+	err4 := dictionary.Update("second", "second update word")
+	if err4 != nil {
+		fmt.Println(err4)
+	} else {
+		fmt.Println(dictionary)
+	}
+
+	err5 := dictionary.Delete("second")
+	if err5 != nil {
+		fmt.Println(err5)
+	} else {
+		fmt.Println(dictionary)
 	}
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -6,6 +6,9 @@ import "errors"
 type Dictionary map[string]string
 
 var errNotFound = errors.New("NOT FOUND")
+var errWordExists = errors.New("THAT WORD ALREADY EXIST")
+var errCantUpdate = errors.New("CANT UPDATE")
+var errCantDelete = errors.New("CANT DELETE")
 
 //Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -14,4 +17,40 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add for a word
+func (d Dictionary) Add(word string, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = definition
+	case nil:
+		return errWordExists
+	}
+	return nil
+}
+
+// Update for a word
+func (d Dictionary) Update(word string, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+// Delete for a word
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		delete(d, word)
+	case errNotFound:
+		return errCantDelete
+	}
+	return nil
 }
