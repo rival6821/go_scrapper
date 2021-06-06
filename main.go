ae31c367ff79c938058626689276c8d6fef0321f
ilhoon <rival6821@naver.com>
2021-06-06 18:07:57 +0900

    go routine, channel

--- main.go
+++ main.go
@@ -1,34 +1,51 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 )
 
-var errRequestFailed = errors.New("REQUEST ERROR")
+type requestResult struct {
+	url    string
+	status string
+}
 
 func main() {
+	results := make(map[string]string)
+
+	c := make(chan requestResult)
+
 	urls := []string{
 		"https://naver.com",
 		"https://ilhoon.kr",
 		"https://youtube.com",
+		"https://nomadcoders.co",
+		"https://server.ilhoon.kr",
+		"https://land.naver.com",
+		"https://comic.naver.com/index.nhn",
+		"https://www.clien.net/service/board/park",
 	}
+
 	for _, url := range urls {
-		err := hitURL(url)
-		if err != nil {
-			fmt.Println(err)
-		}
+		go hitURL(url, c)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
+func hitURL(url string, c chan<- requestResult) {
+	// fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	} else if resp.StatusCode >= 400 {
-		return errRequestFailed
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
 	}
-	return nil
+	c <- requestResult{url: url, status: status}
 }
